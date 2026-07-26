45642e1e721dba31a19c6c084c9c7ba8e06ed4e5
AugustP748 <augustoxx2015@gmail.com>
2026-07-26 20:32:08 -0300

    Configure building to docs/ for GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,19 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//   ],
+// })
+
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  // Reemplaza 'nombre-de-tu-repositorio' por el nombre exacto de tu repo en GitHub
+  base: '/fintech-data-analysis/', 
+  build: {
+    outDir: '../docs',       // Compila en la carpeta 'docs' en la raíz del proyecto
+    emptyOutDir: true,       // Limpia la carpeta docs antes de compilar
+  }
 })
